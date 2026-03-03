d9c377061373bdaf14ea7ab29f1dd16887d6af03
Sofiia Moiseienko <sofiamouseenko@gmail.com>
2026-03-03 22:31:42 +0300

    Update page.tsx

--- app/(private routes)/notes/filter/@content/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/@content/[...slug]/page.tsx
@@ -1,20 +1,18 @@
-import { fetchNotes } from '@/lib/api';
+import { fetchNotes } from '@/lib/api/serverApi';
 import NoteList from '@/components/NoteList/NoteList';
+
 export default async function ContentPage({ params }: { params: Promise<{ tag: string[] }> }) {
   const { tag } = await params;
   const selectedTag = tag[0];
-  
-  let data;
-  
-  if (selectedTag === 'all') {
-    data = await fetchNotes(1, 12, '');
-  } else {
-    data = await fetchNotes(1, 12, '', selectedTag);
-  }
+
+  const data = await fetchNotes(
+    selectedTag === 'all' ? {} : { tag: selectedTag }
+  );
+
   return (
-  <div>
-      {data.notes && data.notes.length > 0 ? (
-        <NoteList notes={data.notes} />
+    <div>
+      {data && data.length > 0 ? (
+        <NoteList notes={data} />
       ) : (
         <p>No notes found</p>
       )}

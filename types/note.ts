4996d346f7913ec8ab77c879613a1978ece1a6bd
Sofiia Moiseienko <sofiamouseenko@gmail.com>
2026-03-03 22:44:47 +0300

    Update note.ts

--- types/note.ts
+++ types/note.ts
@@ -2,6 +2,8 @@ export interface Note {
   id: string;
   title: string;
   content: string;
-  tag: string;
+  tag: NoteTag;
   createdAt: string;
 }
+
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
